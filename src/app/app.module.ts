d35d33f6e2478015150f90e8ec6481038c5ad25c
adityarastogi1212 <adityarastogi1212@gmail.com>
2021-04-02 22:55:52 +0530

    First Commit Message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FriendComponent } from './friend/friend.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FriendComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '',component: FriendComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
